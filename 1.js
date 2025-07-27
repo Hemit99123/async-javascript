ff2c1eb0821093f9a84025dbc0fa26fab942a38a
Hemit Patel <hemitvpatel@gmail.com>
2025-07-26 21:28:19 -0400

    update async example 1

--- 1.js
+++ 1.js
@@ -1,1 +1,16 @@
+function runProgram() {
+  console.log("Task 1 complete")
 
+  async function fetchData() {  
+    return "Fetch the data"
+  }
+
+  fetchData()
+    .then((data) => {
+      console.log(data)
+    })
+
+  console.log("Task 2 complete")
+}
+
+runProgram()
