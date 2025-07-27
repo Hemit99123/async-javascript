77db417241c704c28e3b58c5172becc1d41df035
Hemit Patel <hemitvpatel@gmail.com>
2025-07-26 21:53:24 -0400

    throw an error in the async function (for promise to use)

--- 1.js
+++ 1.js
@@ -1,13 +1,30 @@
 function runProgram() {
   console.log("Task 1 complete")
 
-  async function fetchData() {  
-    return "Fetch the data"
+  async function fetchData(success) {
+    if (success) {
+      return "Fetch the data"
+    } else {
+      throw new Error("Something went wrong while fetching the data");
+    }
   }
 
-  fetchData()
-    .then((data) => {
+  // Execution 1
+  fetchData(true)
+    .then(data => {
       console.log(data)
+    })
+    .catch(error => {
+      console.log(error.message)
+    })
+
+  // Execution 2
+  fetchData(false)
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.log(error.message)
     })
 
   console.log("Task 2 complete")
